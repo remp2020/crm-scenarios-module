444fa61e64dedc9b87923dc9dcca76e4799f11c6
Miroslav Svítok <mirosvitok@gmail.com>
2019-05-14 14:47:19 +0200

    Enabled flag in scenarios table

---
+++ src/migrations/20190514124435_add_enabled_to_scenarios.php
@@ -0,0 +1,13 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddEnabledToScenarios extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table('scenarios')
+            ->addColumn('enabled', 'boolean', ['default' => false, 'null' => false])
+            ->save();
+    }
+}
