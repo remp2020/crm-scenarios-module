c080f029752e7fa29f8e798d8723125b2a027e0c
Miroslav Svítok <mirosvitok@gmail.com>
2019-05-23 13:23:24 +0200

    Truncate scenarios data to avoid data conversions
    Scenarios data was not in real use at the moment, so we can skip its conversion.
    
    remp/crm#683

--- src/migrations/20190523111224_alter_type_enum_in_scenarios_elements.php
+++ src/migrations/20190523111224_alter_type_enum_in_scenarios_elements.php
@@ -6,6 +6,15 @@ class AlterTypeEnumInScenariosElements extends AbstractMigration
 {
     public function change()
     {
+        // Delete all scenarios and related tables to avoid converting data (scenarios were not in real use at the moment)
+        $this->query('SET foreign_key_checks = 0');
+        $this->table('scenarios_element_elements')->truncate();
+        $this->table('scenarios_trigger_elements')->truncate();
+        $this->table('scenarios_elements')->truncate();
+        $this->table('scenarios_triggers')->truncate();
+        $this->table('scenarios')->truncate();
+        $this->query('SET foreign_key_checks = 1');
+
         $this->table('scenarios_elements')
             ->changeColumn('type', 'enum', [
                 'null' => false,
