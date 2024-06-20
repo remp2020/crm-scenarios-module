37ef2d9fbec1f71337d565eab20cf93d6688a493
Martin Brieška <martin.brieska@fatchillimedia.com>
2024-06-20 11:24:12 +0200

    Add scenario criterion UserCreatedAtCriteria
    Filters users created before or "in the last" timeframe. Criterion can be
    used within `condition` node, select `user` and `created at`.
    
    Added was also TimeframeScenarioTrait which can be used within implementation
    of `ScenariosCriteriaInterface->addConditions()` if parameter `TimeframeParam`
    is part of definition. It validated selected operator, units and value
    and returns limit and operator which can be used further within
    selection.
    
    Co-authored-by: Dominik Skerhak <dominik.skerhak@gmail.com>
    
    remp/remp#2998

---
+++ src/Scenarios/TimeframeScenarioTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Crm\ScenariosModule\Scenarios;
+
+use Nette\Utils\DateTime;
+use Nette\Utils\Json;
+
+trait TimeframeScenarioTrait
+{
+    /**
+     * @param string[] $units
+     * @param string[] $operators
+     * @return null|array{limit: \Nette\Utils\DateTime|false, operator: string}
+     */
+    protected function getTimeframe(array $paramValues, array $units, array $operators, string $timeFrameKey): ?array
+    {
+        if (isset(
+            $paramValues[$timeFrameKey],
+            $paramValues[$timeFrameKey]->operator,
+            $paramValues[$timeFrameKey]->unit,
+            $paramValues[$timeFrameKey]->selection
+        )) {
+            $timeframeOperator = array_search($paramValues[$timeFrameKey]->operator, $operators, true);
+            if ($timeframeOperator === false) {
+                throw new \Exception("Timeframe operator [{$timeframeOperator}] is not a valid operator out of: " . Json::encode(array_values($operators)));
+            }
+            $timeframeUnit = $paramValues[$timeFrameKey]->unit;
+            if (!in_array($timeframeUnit, $units, true)) {
+                throw new \Exception("Timeframe unit [{$timeframeUnit}] is not a valid unit out of: " . Json::encode($units));
+            }
+            $timeframeValue = $paramValues[$timeFrameKey]->selection;
+            if (filter_var($timeframeValue, FILTER_VALIDATE_INT, array("options" => array("min_range" => 0))) === false) {
+                throw new \Exception("Timeframe value [{$timeframeValue}] is not a valid value. It has to be positive integer.");
+            }
+
+            $limit = (new DateTime())->modify('-' . $timeframeValue . $timeframeUnit);
+            $operator = $operators[$timeframeOperator];
+
+            return [
+                'limit' => $limit,
+                'operator' => $operator,
+            ];
+        }
+
+        return null;
+    }
+}
