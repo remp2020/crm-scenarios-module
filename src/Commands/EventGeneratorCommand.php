bb906aed5b1cdf1737aad21fe1d5a9de12c3b243
Dominik Skerhak <dominik.skerhak@gmail.com>
2025-05-13 16:53:31 +0200

    Beautified output of EventGeneratorCommand
    - (Breaking) Changed format of returned array from
      `BeforeEventGenerator#generate()`.
    - (Log) output of `EventGeneratorCommand` (`scenarios:event_generator`)
      was beautified.
    
    remp/respekt#390

--- src/Commands/EventGeneratorCommand.php
+++ src/Commands/EventGeneratorCommand.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace Crm\ScenariosModule\Commands;
 
+use Crm\ApplicationModule\Commands\DecoratedCommandTrait;
 use Crm\ScenariosModule\Events\BeforeEventGenerator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -9,13 +11,12 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class EventGeneratorCommand extends Command
 {
-    private $beforeEventGenerator;
+    use DecoratedCommandTrait;
 
-    public function __construct(BeforeEventGenerator $beforeEventGenerator)
-    {
+    public function __construct(
+        private BeforeEventGenerator $beforeEventGenerator,
+    ) {
         parent::__construct();
-
-        $this->beforeEventGenerator = $beforeEventGenerator;
     }
 
     protected function configure()
@@ -26,15 +27,20 @@ class EventGeneratorCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Generating before events - STARTED");
-
         $results = $this->beforeEventGenerator->generate();
 
-        foreach ($results as $code => $events) {
-            $output->writeln(count($events) . " events generated for trigger: {$code}");
+        ksort($results);
+        foreach ($results as $code => $eventTimeOffsets) {
+            ksort($eventTimeOffsets);
+            foreach ($eventTimeOffsets as $minutes => $events) {
+                $output->writeln(
+                    " * <comment>" . count($events) . " events</comment> generated for trigger: "
+                    . "<comment>{$code}</comment> (time offset: <comment>{$minutes}</comment> minutes)"
+                );
+            }
         }
 
-        $output->writeln("Generating before events - DONE");
+        $output->writeln('Done');
 
         return Command::SUCCESS;
     }
