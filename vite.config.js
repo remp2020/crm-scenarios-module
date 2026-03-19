0e89c05edae3491cc77cca0d796354978eba987e
Miroslav Svitok <mirosvitok@gmail.com>
2026-03-19 13:58:44 +0000

    Change JS build tool to Vite, upgrade of JS dependencies
    remp/crm#3662

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'src/assets/scenario-builder/dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  server: {
+    port: 3030,
+  },
+});
