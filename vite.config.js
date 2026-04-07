f974063878c59076cf18a685ecdccfa319e693aa
Peter Dulacka <dulacka@gmail.com>
2026-04-07 09:53:59 +0200

    Minor post-release fixes
    - Fix seeder output to actually print code of the gateway
    - Fix invalid import of dependencies in test.
    - Fix indentation of vite config in scenarios module.

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'src/assets/scenario-builder/dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-      },
+    plugins: [react()],
+    build: {
+        outDir: 'src/assets/scenario-builder/dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]',
+            },
+        },
     },
-  },
-  server: {
-    port: 3030,
-  },
+    server: {
+        port: 3030,
+    },
 });
